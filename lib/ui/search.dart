3c091735401d0c0657f006054841de72861844bd
Udith <udithshalinda2@gmail.com>
2019-07-20 17:16:34 +0530

    searching page fixed

--- lib/ui/search.dart
+++ lib/ui/search.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:my_stackoverflow/modle/Question.dart';
 import 'package:my_stackoverflow/modle/user.dart';
 import 'package:my_stackoverflow/ui/signup.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -21,6 +22,8 @@ class SearchState extends State<Search> {
   final FirebaseDatabase database = FirebaseDatabase.instance;
   DatabaseReference databaseReference;
   var searchKey = new TextEditingController();
+  String filter;
+  List<Question> questionList = new List();
   
   
 
@@ -28,8 +31,9 @@ class SearchState extends State<Search> {
   void initState() {
     super.initState();
     searchKey.text = "";
-
-    databaseReference = database.reference().child("userDetails");
+    databaseReference = database.reference().child("Questions");
+    databaseReference.onChildAdded.listen(setQuestions);
+    // filter = "";
   }
 
   @override
@@ -56,7 +60,7 @@ class SearchState extends State<Search> {
               suffixIcon:IconButton(
                 icon: Icon(Icons.search),
                 color: Colors.white,
-                onPressed: (){},
+                onPressed: searchButtonPressed,
                 ),
                 hintText: "Enter the Name",
                 hintStyle: TextStyle(
@@ -70,7 +74,8 @@ class SearchState extends State<Search> {
               ),
             ),
           ),
-          
+          //start of the result
+          resultBody(),
         ],
 
       ),
@@ -80,85 +85,43 @@ class SearchState extends State<Search> {
 
     );
   }
- 
-  Widget searchResultBody(){
-    return Container(
-                  child : new ListView(
-                    children: <Widget>[
-                      new Container(
-                        height:  MediaQuery.of(context).size.height-50,
-                        child: new FirebaseAnimatedList(
-                            query: databaseReference,
-                            itemBuilder: (_, DataSnapshot snapshot,Animation<double> animation , int index){
-                              return new Card(
-                                  elevation: 8.0,
-                                  margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-                                  child: new Container(
-                                    decoration: BoxDecoration(color: Colors.white),
-                                    child: new ListTile(
-                                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-//                                      leading: Container(
-//                                        padding: EdgeInsets.only(right: 12.0),
-////                                        decoration: new BoxDecoration(
-////                                            border: new Border(
-////                                                right: new BorderSide(width: 1.0, color: Colors.white24))),
-//                                        child: CircleAvatar(
-//                                          radius: 30.0,
-//                                          backgroundColor: Colors.white,
-//                                          child: new Text(snapshot.value['email'].substring(5,10)),
-//                                        ),
-//                                      ),
-                                      title: Text("title"),
-                                      subtitle: Container(
-                                        alignment: FractionalOffset.topLeft,
-                                        padding: EdgeInsets.only(top: 30),
-                                        child:Column(
-                                          children: <Widget>[
-                                            Row(
-                                              children: <Widget>[
-                                                // Flexible(
-                                                //   child:voteupdown(snapshot,index),
-                                                // ),
-                                                Flexible(
-                                                  child: questionandAnswer(snapshot),
-                                                ),
-                                              ],
-                                            ),
-                                            // buttonSet(snapshot,index),
-                                          ],
-                                        ),
-                                      ),
-                                      trailing: new Icon(Icons.arrow_right, color: Colors.grey, size: 50.0),
-
-                                      onTap: (){
-                                        showQuestion(snapshot.key);
-                                      },
-                                    ),
-                                  )
-                              );
-                            }
-                        ),
-                      ),
-                    ],
-                  )
-              );
+  void setQuestions(Event event){
+    questionList.add(Question.fromSnapshot(event.snapshot));
   }
-  Widget questionandAnswer(DataSnapshot snapshot){
-      return Column(
-        children: <Widget>[
-          Container(
-            child: Text(
-              snapshot.value['question'].toString(),
-              overflow: TextOverflow.fade,
-              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold,fontSize: 25),
+  Widget testSearchResult(){
+    return Flexible(
+            child: FirebaseAnimatedList(
+              query: database.reference().child("Questions"),
+              itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                  Animation<double> animation, int index) {
+                return  questionList[index].question.contains(filter) || questionList[index].description.contains(filter) ? ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text(
+                    questionList[index].question,
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  subtitle: Text
+                  (questionList[index].description,
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                  onTap:(){
+                     showQuestion(questionList[index].key);
+                  },
+                ) : new Container();
+              },
             ),
-          ),
-          Text(
-            snapshot.value['question'].toString(),
-            style: TextStyle(color: Colors.blueGrey, fontSize: 15),
-          ),
-        ],
-      );
+          );
+  }
+ 
+ 
+  Widget resultBody(){
+    if(filter != null){
+      return testSearchResult();
+    }else{
+      return Center(
+        child: Text("Search a question"),
+        );
+    }
   }
 
   void showQuestion(String key){
@@ -169,4 +132,9 @@ class SearchState extends State<Search> {
     Navigator.of(context).push(router);
   }
   
+  searchButtonPressed(){
+    setState(() {
+     filter = searchKey.text; 
+    });
+  }
 }
